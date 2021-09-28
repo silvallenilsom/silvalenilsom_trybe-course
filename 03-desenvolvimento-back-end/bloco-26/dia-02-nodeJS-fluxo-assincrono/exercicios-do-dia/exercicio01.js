fd3cdac586642e7ee614ab728298c72eaf9bbab8
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-09-28 13:19:50 -0300

    Adicionnado dias 1 e 2

---
+++ 03-desenvolvimento-back-end/bloco-26/dia-02-nodeJS-fluxo-assincrono/exercicios-do-dia/exercicio01.js
@@ -0,0 +1,15 @@
+function calculate(n1, n2, n3) {
+  return new Promise((resolve, reject) => {
+    if (typeof n1 !== 'number' || n2 !== 'number' || n3 !== 'number') {
+      reject('informe apenas numeros');
+    }
+
+    const resultado = ((n1 + n2) * n3);
+    if (resultado < 50) {
+      reject('Valor inferior a 50');
+    }
+
+    resolve(resultado);
+
+  });
+}
